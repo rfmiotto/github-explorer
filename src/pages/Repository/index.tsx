75b2e8ed2642deef342bb881bea4c6c98e47b378
rfmiotto <renato.fmiotto@gmail.com>
2020-05-02 15:21:48 -0300

    Link to github repo

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RouteParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RouteParams>();
+
+  return <h1>Repository:{params.repository}</h1>;
 };
 
 export default Repository;
