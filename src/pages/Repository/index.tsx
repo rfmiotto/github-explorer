54c2611c2de64a6fe36955db5f05f3a8994498d5
rfmiotto <renato.fmiotto@gmail.com>
2020-05-02 17:41:54 -0300

    Add style to repo page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RouteParams {
   repository: string;
@@ -8,7 +12,57 @@ interface RouteParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RouteParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          Return
+          <FiChevronLeft size={16} />
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/35971460?s=460&u=afbd61f4c934845811d218d94e094963081d5a30&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>repo name</strong>
+            <p>description</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>1800</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>1800</strong>
+            <span>Open issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aefaefae">
+          <div>
+            <strong>aeaeae</strong>
+            <p>Afaefaefa</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
