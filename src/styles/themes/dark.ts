feda31424766bb400f3ff535d3128b4401c9aa1f
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2020-12-26 12:07:15 -0300

    Add toggle theme - dark and light

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    background: '#2d3142',
+    primary: '#4f5d75',
+    secundary: '#bfc0c0',
+    text: '#ffffff',
+    inputText: '#495867',
+  },
+};
